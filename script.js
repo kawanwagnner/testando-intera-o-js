45bb24e93bdc5718e8a981bb58af6f440473f11f
Kawan Wagnner <103159338+kawanwagnner@users.noreply.github.com>
2022-12-06 16:22:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('#send')
+
+btn.addEventListener('click', () => {
+
+    const nomeCliente = document.querySelector('#name')
+    const substituicao = document.querySelector('.substituicao')
+    const Client = String(nomeCliente.value)
+    
+    substituicao.innerHTML = Client
+});
